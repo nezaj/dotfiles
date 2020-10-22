daf590d00e2fb6e3ab2d9d394a575aa96ab00377
Joe Averbukh <joeaverbukh@gmail.com>
2020-10-22 11:36:28 -0400

    Add clojure

--- emacs/init.el
+++ emacs/init.el
@@ -148,7 +148,7 @@
  ;; If there is more than one, they won't work right.
  '(coffee-tab-width 2)
  '(package-selected-packages
-   '(evil with-shell-interpreter magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell)))
+   '(## evil with-shell-interpreter magit tagedit rainbow-delimiters projectile smex ido-completing-read+ cider clojure-mode-extra-font-locking clojure-mode paredit exec-path-from-shell)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
