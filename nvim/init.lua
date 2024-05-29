8dd6b063c4377b9f3d8f0edb740ce252c9ae1477
Joe Averbukh <joeaverbukh@gmail.com>
2024-05-29 16:18:46 -0400

    copyable ts error

--- nvim/init.lua
+++ nvim/init.lua
@@ -335,13 +335,20 @@ lsp.on_attach(function(client, bufnr)
   vim.keymap.set("n", "gi", function() vim.lsp.buf.implementation() end, opts)
   vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
   vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-  vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
   vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
   vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
   vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
   vim.keymap.set("n", "<leader>gr", function() vim.lsp.buf.references() end, opts)
   vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
   vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+  vim.keymap.set("n", "<leader>vd", function()
+      local opts = {
+          scope = "cursor",
+          focusable = true, -- Allow focusing the float window to enable copying text
+          border = "rounded" -- Optional: style the border of the float window
+      }
+      vim.diagnostic.open_float(nil, opts)
+  end, opts)
 end)
 
 lsp.setup()
