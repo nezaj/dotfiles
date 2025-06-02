16a6174c7d12e30da3e23df853793750e316eea4
Joe Averbukh <joeaverbukh@gmail.com>
2025-06-02 15:32:19 -0700

    Fix tab complete

--- nvim/init.lua
+++ nvim/init.lua
@@ -437,7 +437,6 @@ require("lazy").setup({
       local cmp = require('cmp')
       local luasnip = require('luasnip')
       require('luasnip.loaders.from_vscode').lazy_load()
-
       cmp.setup {
         snippet = { expand = function(args) luasnip.lsp_expand(args.body) end },
         mapping = {
@@ -445,6 +444,27 @@ require("lazy").setup({
           ['<C-n>']     = cmp.mapping.select_next_item(cmp.SelectBehavior.Select),
           ['<C-y>']     = cmp.mapping.confirm({ select = true }),
           ['<C-Space>'] = cmp.mapping.complete(),
+
+          -- Add Tab for cycling
+          ['<Tab>']     = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_jumpable() then
+              luasnip.expand_or_jump()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+
+          ['<S-Tab>']   = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
         },
         sources = cmp.config.sources(
           { { name = 'nvim_lsp' }, { name = 'luasnip' } },
